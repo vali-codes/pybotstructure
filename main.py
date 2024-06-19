93ed71b701e680e8fd14fd1bc2a29d4d203eb3bd
valicodes <valentin.martin.05@icloud.com>
2024-06-19 20:27:11 +0200

    Added: - .env - startup of the bot

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import discord
+import os
 
-client = discord.Client()
-
-
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-    
+
+
+@client.command()
+async def ping(ctx):
+    embed = discord.Embed(
+        title= "Bot Ping",
+        description= f'Ping: {round(client.latency * 1000)}ms',
+        color= discord.Color.blue()
+    )
+    await ctx.send(embed=embed)
+
+client.run(os.environ.get('TOKEN'))
+
+
